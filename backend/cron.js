edc731fccbe9ddd6bc5785def20f447679f646d5
devarifkhan <arifcse209@gmail.com>
2026-02-22 15:38:27 +0600

    chore: update URL in cron job and add vercel configuration for rewrites

--- backend/cron.js
+++ backend/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron";
 import https from "https";
 
-const URL = "https://graphql-crash-course.onrender.com";
+const URL = "https://expense-tracker-graphql-pc0j.onrender.com";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
 	https
